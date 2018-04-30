03f271a20d0ed4ffc6308540f6feb674a4262109
Emanuele Leoni <emanuele@pagita.com>
2018-04-30 19:19:14 +0200

    Introdotto metodo per json rpc

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using Microsoft.Extensions.Localization;
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace LK2.Controllers
 {
@@ -23,7 +26,7 @@ namespace LK2.Controllers
         /// <param name="categoryProductRepository"></param>
         /// <param name="productRepository"></param>
         public HomeController(IStringLocalizer<HomeController> localizer,
-                              ICategoryProductRepository categoryProductRepository, 
+                              ICategoryProductRepository categoryProductRepository,
                               IProductRepository productRepository)
         {
             _localizer = localizer;
@@ -38,21 +41,22 @@ namespace LK2.Controllers
         /// <returns></returns>
         [HttpGet]
         public IActionResult Index()
-        {            
+        {
             ViewBag.Categories = repoCat.GetList(1);
             ViewBag.Products = repoProd.GetList(1);
 
             return View();
         }
 
-        public void Python(){
+        public void Python()
+        {
             try
             {
                 ProcessStartInfo start = new ProcessStartInfo();
 
                 start.FileName = @"C:\Users\emanl\AppData\Local\Programs\Python\Python36-32\python.exe"; //full path to python.exe
-                                                            //start.FileName = @"C:\Windows\system32\cmd.exe";
-                                                            //start.Arguments = string.Format("{0} {1} {2} {3}", @"C:\Users\J1035\Documents\Python27\GiveX_python\test.py", "123456789", "603628982592000186162", 20.00);
+                                                                                                         //start.FileName = @"C:\Windows\system32\cmd.exe";
+                                                                                                         //start.Arguments = string.Format("{0} {1} {2} {3}", @"C:\Users\J1035\Documents\Python27\GiveX_python\test.py", "123456789", "603628982592000186162", 20.00);
                 start.Arguments = @"C:\Users\emanl\Downloads\HelloWorld.py";
 
                 start.UseShellExecute = false;
@@ -70,6 +74,28 @@ namespace LK2.Controllers
             {
                 var foo = ex.Message;
             }
+        }
+
+        public async Task CallTestService()
+        {
+            UTF8Encoding enc = new UTF8Encoding();
+            string data = "{ \"method\" : \"ping\", \"id\" : \"d70f2bbb - 4540 - 4876 - a665 - adae32d7fa53\" }";
+
+            //Create request
+            WebRequest request = WebRequest.Create("http://34.241.55.104:8080");
+            request.Method = "POST";
+            request.ContentType = "application/json";
+
+            //Set data in request
+            Stream dataStream = await request.GetRequestStreamAsync();
+            dataStream.Write(enc.GetBytes(data), 0, data.Length);
+
+
+            //Get the response
+            WebResponse wr = await request.GetResponseAsync();
+            Stream receiveStream = wr.GetResponseStream();
+            StreamReader reader = new StreamReader(receiveStream, Encoding.UTF8);
+            string content = reader.ReadToEnd();
         }
 
         [HttpPost]
